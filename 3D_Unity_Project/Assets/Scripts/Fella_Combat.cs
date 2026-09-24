f3b4a3cfec34bcf012757b875573f4ffdb1866a5
Jessikyu <10931250@uvu.edu>
2026-09-23 21:45:02 -0600

    Some work on the game dev 1 game

---
+++ 3D_Unity_Project/Assets/Scripts/Fella_Combat.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Fella_Combat : MonoBehavior
+{
+    
+    void update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Left_Click))  {}
+    }
+
+}
